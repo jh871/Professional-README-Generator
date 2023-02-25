99f1792c29bd1ab2bb62b4fcaba5f61b1c8fb9ab
jh871 <jennyh871@gmail.com>
2023-02-25 00:24:07 +0000

    generateMarkdown generates markdown via index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+
 // array of questions for user
 const questions = [{
     type: 'input',
@@ -13,54 +14,64 @@ const questions = [{
     type: 'input',
     name: 'description',
     message: 'Please provide a short description of your application: '
-    },
-    {
-    type: 'input',
-    name: 'installation',
-    message: 'Please provide guidance on how the application can be installed: '
-    },
-    {
-    type: 'input',
-    name: 'usage',
-    message: 'Please provide information on how the application should be used: '
-    },
-    {
-    type: 'list',
-    name: 'license',
-    message: 'Please select the license for you application:'
-    },
-    {
-    type: 'input',
-    name: 'contributors',
-    message: 'Please list anyone else who collaborated on this project:'
-    },
-    {
-    type: 'input',
-    name: 'tests',
-    message: '??????????'
-    },
-    {
-    type: 'input',
-    name: 'questions_github',
-    message: 'Please enter your gitHub username:'
-    },
-    {
-    type: 'input',
-    name: 'questions_email',
-    message: 'Please enter your email address:'
-    },
-
-
+    }
 ];
 
 // function to write README file
-function writeToFile(`${title}.md`, data) {
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Okay!!!"));
 }
+
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((data) => {
+        writeToFile('test.md', generateMarkdown.generateMarkdown(data))
+    });
 }
 
 // function call to initialize program
 init();
+
+
+/*
+(err) =>
+    err ? console.error(err) : console.log("check for file!")
+*/
+
+    // {
+    // type: 'input',
+    // name: 'installation',
+    // message: 'Please provide guidance on how the application can be installed: '
+    // },
+    // {
+    // type: 'input',
+    // name: 'usage',
+    // message: 'Please provide information on how the application should be used: '
+    // },
+    // {
+    // type: 'list',
+    // name: 'license',
+    // message: 'Please select the license for you application:'
+    // },
+    // {
+    // type: 'input',
+    // name: 'contributors',
+    // message: 'Please list anyone else who collaborated on this project:'
+    // },
+    // {
+    // type: 'input',
+    // name: 'tests',
+    // message: '??????????'
+    // },
+    // {
+    // type: 'input',
+    // name: 'questions_github',
+    // message: 'Please enter your gitHub username:'
+    // },
+    // {
+    // type: 'input',
+    // name: 'questions_email',
+    // message: 'Please enter your email address:'
+    // },
