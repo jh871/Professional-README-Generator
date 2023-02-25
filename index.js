8592ebccea98b71f0242a014f32b30465fd0ae13
jh871 <jennyh871@gmail.com>
2023-02-25 18:37:10 +0000

    readme has working links. Instructions added to inquirer.

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [{
     type: 'input',
+    name: 'instructions',
+    message: 'To enter your answers please type the text in the prose you expect it to appear. WARNING: Pressing the Enter key while typing an answer will move to the next question. To generate a new line use <br>. To use bullet points: use "<br> - " with <br> after each point and a space after the hyphen, or use numbered points (1. abc <br>2. def <br>3. xyz) with <br> after each point. Press Enter to continue.'
+    },
+    {
+    type: 'input',
     name: 'title',
     message: 'What is the title of your project?'
     },
@@ -14,64 +19,58 @@ const questions = [{
     type: 'input',
     name: 'description',
     message: 'Please provide a short description of your application: '
-    }
+    },
+    {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide guidance on how the application can be installed: '
+    },
+    {
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide information on how the application should be used: '
+    },
+    {
+    type: 'list',
+    name: 'license',
+    message: 'Please select the license for you application:',
+    choices: ['License 1', 'License 2']
+    },
+    {
+    type: 'input',
+    name: 'contributors',
+    message: 'Please list anyone else who collaborated on this project:'
+    },
+    {
+    type: 'input',
+    name: 'tests',
+    message: '??????????'
+    },
+    {
+    type: 'input',
+    name: 'questions_github',
+    message: 'Please enter your gitHub username:'
+    },
+    {
+    type: 'input',
+    name: 'questions_email',
+    message: 'Please enter your email address:'
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log("Okay!!!"));
-}
+    err ? console.error(err) : console.log("Check current directory for file!"));
+};
 
 
 // function to initialize program
 function init() {
     inquirer.prompt(questions).then((data) => {
-        writeToFile('test.md', generateMarkdown.generateMarkdown(data))
+        writeToFile('test1.md', generateMarkdown.generateMarkdown(data))
     });
-}
+};
 
 // function call to initialize program
 init();
-
-
-/*
-(err) =>
-    err ? console.error(err) : console.log("check for file!")
-*/
-
-    // {
-    // type: 'input',
-    // name: 'installation',
-    // message: 'Please provide guidance on how the application can be installed: '
-    // },
-    // {
-    // type: 'input',
-    // name: 'usage',
-    // message: 'Please provide information on how the application should be used: '
-    // },
-    // {
-    // type: 'list',
-    // name: 'license',
-    // message: 'Please select the license for you application:'
-    // },
-    // {
-    // type: 'input',
-    // name: 'contributors',
-    // message: 'Please list anyone else who collaborated on this project:'
-    // },
-    // {
-    // type: 'input',
-    // name: 'tests',
-    // message: '??????????'
-    // },
-    // {
-    // type: 'input',
-    // name: 'questions_github',
-    // message: 'Please enter your gitHub username:'
-    // },
-    // {
-    // type: 'input',
-    // name: 'questions_email',
-    // message: 'Please enter your email address:'
-    // },
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,27 +5,34 @@ return `# ${data.title}
 ## Description
 ${data.description}
 
+## Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributors](#contributors)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This application is covered under a ${data.license} license. Please see the repo for full license.
+
+## Contributors
+${data.contributors}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions about this application please see my repo at https://github.com/${data.questions_github} or email me at ${data.questions_email}
+
 `;
 }
 
 module.exports = { generateMarkdown };
 
-// ## Installation
-// ${installation}
-//
-// ## Usage
-// ${usage}
-//
-// ## License
-// ${license}
-//
-// ## Contributors
-// ${contributors}
-//
-// ## Tests
-// ${tests}
-//
-// ## Questions
-// If you have any questions about this application 
-// please see my repo at https://github.com/${questions_github}
-// or email me at ${questions_email}
